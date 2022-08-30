84e4ddc25449f182c92a415ee6344e5ce495b733
h.mostafavi <mostafaviheris.hosein@gmail.com>
2022-08-30 18:32:16 +0430

    sd

--- src/main/java/ir/storming/service/InvalidPasswordException.java
+++ src/main/java/ir/storming/service/InvalidPasswordException.java
@@ -4,7 +4,7 @@ public class InvalidPasswordException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public InvalidPasswordException() {
+     public InvalidPasswordException() {
         super("Incorrect password");
     }
 
